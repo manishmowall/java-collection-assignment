8f1de6a92038e38b8c47ee63fa7174941fd70b39
nitishm-webonise <nitish.mowall@weboniselab.com>
2016-09-19 10:43:19 +0530

    added hashcode function ,equals function  and comparator class

--- src/main/java/org/nitishm/javacollectionassignment/DateOfBirthComparator.java
+++ src/main/java/org/nitishm/javacollectionassignment/DateOfBirthComparator.java
@@ -6,7 +6,6 @@ public class DateOfBirthComparator implements Comparator {
     public int compare(Object o1, Object o2){
         User user1 = (User) o1;
         User user2 = (User) o2;
-
         return user1.getEmail().compareTo(user2.getEmail());
     }
 }
